d3df15d95e178a6a1707169d8efae333da2a6b56
Nicolas Berger <nicoberger@gmail.com>
2014-07-02 21:40:01 -0300

    Show a map in the page

--- src/rapipago_simple_map/core.cljs
+++ src/rapipago_simple_map/core.cljs
@@ -1,5 +1,15 @@
-(ns rapipago_simple_map.core)
+(ns rapipago_simple_map.core
+  )
 
 (enable-console-print!)
 
-(println "Hello world!")
+(def map-options #js {
+                      :div "#map"
+                      :zoom 14
+                      :lat -34.603272
+                      :lng -58.396726
+                      :click #(js/alert "click")
+                      :dragend #(js/alert (str "dragend" %))
+                      })
+
+(def gmap (js/GMaps. map-options))
